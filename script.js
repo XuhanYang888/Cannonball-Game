4e733859cc06f6f516c5ac573c510dc93ebfca8a
XuhanY <xuhany@protonmail.com>
2024-05-03 21:07:47 -0400

    aaa

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ function changeRes() {
     console.log("aa");
     var canvas = document.getElementById("defaultCanvas0");
     canvas.style.width = String(Math.floor(1600/ratio)) + "px";
+    canvas.style.height = String(Math.floor(900/ratio)) + "px";
 }
