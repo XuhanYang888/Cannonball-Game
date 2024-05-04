e1459b9efccfc4cc084c11415d525e0e8b455dcf
XuhanY <xuhany@protonmail.com>
2024-05-03 21:14:20 -0400

    aa

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 var ratio = window.devicePixelRatio;
 
 function changeRes() {
-    console.log("aa");
-    var canvas = document.getElementById("defaultCanvas0");
-    canvas.style.width = String(Math.floor(1600/ratio)) + "px";
-    canvas.style.height = String(Math.floor(900/ratio)) + "px";
+    const canvas = document.getElementById("defaultCanvas0");
+    canvas.style.width = String(Math.floor(1600 / ratio)) + "px";
+    canvas.style.height = String(Math.floor(900 / ratio)) + "px";
+}
+
+function update() {
+    var height = window.screen.availHeight;
+    var width = window.screen.availWidth;
+    const display = document.getElementById("display");
+    display.innerHTML = height + " " + width;
 }
