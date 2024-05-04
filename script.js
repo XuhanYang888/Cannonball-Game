fab7d0b4ef58b8b3f140547a3cc6f081af94dc68
XuhanY <xuhany@protonmail.com>
2024-05-04 19:13:05 -0400

    big update

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var ratio = window.devicePixelRatio;
+var gamePause = false;
 
 function changeRes() {
-  var height = window.screen.availHeight;
-  var width = window.screen.availWidth;
-  const display = document.getElementById("display");
-  display.innerHTML = height + " " + width;
-  const canvas = document.getElementById("defaultCanvas0");
-  canvas.style.width = String(width - 40) + "px";
-  canvas.style.height = String((width - 40) / 2) + "px";
+    var width = window.screen.availWidth;
+    const canvas = document.getElementById("defaultCanvas0");
+    canvas.focus();
+    canvas.style.width = String(width - 40) + "px";
+    canvas.style.height = String((width - 40) / 2) + "px";
 }
+
+function Pause() {
+    const activeArea = document.activeElement;
+    if (activeArea.id == "defaultCanvas0") {
+        gamePause = false;
+    } else {
+        gamePause = true;
+    }
+}
+window.addEventListener("click", Pause);
