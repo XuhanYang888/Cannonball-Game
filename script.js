2c7cd17fab2a4d38708fe58e327f99e4a121c10a
XuhanY <xuhany@protonmail.com>
2024-05-03 21:05:05 -0400

    aa

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.querySelector("meta[name=viewport]")
-    .setAttribute('content', 'width=device-width, initial-scale='
-        + (1 / window.devicePixelRatio));
+var ratio = window.devicePixelRatio;
+
+function changeRes() {
+    console.log("aa");
+    var canvas = document.getElementById("defaultCanvas0");
+    canvas.style.width = String(Math.floor(1600/ratio)) + "px";
+}
